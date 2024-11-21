49154a73aa8649835286b265813c389adf08122d
florianUnilim <florian.denis@etu.unilim.fr>
2024-11-21 09:04:39 +0100

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,16 @@ import mdx from '@astrojs/mdx';
 
 import sitemap from '@astrojs/sitemap';
 
+import tailwind from '@astrojs/tailwind';
+
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+    site: 'https://FlorianPortfolio.github.io',
+    base:'my-repo',
+    integrations: [mdx(), sitemap(), tailwind(), react()],
 });
+
+
